7c69886662b863feabb655cbff0d1ce876a04ae2
Shinsuke  May <s.seanmay@gmail.com>
2021-04-17 21:03:12 +0900

    add mock test

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import Home from '../Home';
+import { WhoisGetter } from '../services/WhoisGetter'
 import '@testing-library/jest-dom'
 import axios from 'axios';
 import { render, screen, waitFor } from '@testing-library/react';
 
+// const axios = require('axios')
 
+jest.mock('axios')
  
-describe('Home', () => {
-  test('render component and find textbox', () => {
-    render(<Home />);  
-    
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
-    expect(screen.queryByText(/this is a test/)).toBeNull();
-  });
+describe('<Home />', () => {
+  describe('Home component renders', () => {
+    test('page should contain a textbox', () => {
+      render(<Home />);  
+      
+      expect(screen.getByRole('textbox')).toBeInTheDocument();
+      expect(screen.queryByText(/this is a test/)).toBeNull();
+    });
+  })
+
+  describe('Test mocks', () => {
+    test('test returns undefined by default', () => {
+      const append = jest.fn()
+  
+      let result = append("foo")
+  
+      expect(result).toBeUndefined()
+      expect(append).toHaveBeenCalled()
+      expect(append).toHaveBeenCalledTimes(1)
+      expect(append).toHaveBeenCalledWith("foo")
+    });
+  })
+
+  describe('mocking api call', () => {
+    test('returns ', async () => {
+      axios.get.mockResolvedValue({
+        data: [
+          {
+            userId: 1,
+            id: 1,
+            title: "My first title"
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: "Album: sequel"
+          }
+        ]
+      })
+
+      const title = await WhoisGetter('My first title')
+      expect(title).toHaveLength(2)
+    });
+  })
 }) 
